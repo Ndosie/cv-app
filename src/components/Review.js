e5220711db5c28e34f9550006d79f3b1c6b65094
Esther Ndosi <estherndosi24@gmail.com>
2026-03-23 12:21:17 +0300

    Adding downloading feature

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,35 +1,42 @@
+import { usePDF } from "react-to-pdf";
+
 function Review({ formData, prevStep }) {
+  const {toPDF, targetRef} = usePDF({ filename: "CV.pdf" });
+
   return (
-    <div className="review">
-      <h2>Review Your CV</h2>
-      <p>
-        Name: {formData.first_name} {formData.last_name}
-      </p>
-      <h3>Contacts</h3>
-      <p>Email: {formData.email}</p>
-      <p>Phone Number: {formData.phone}</p>
-      <h3>Education</h3>
-      {formData.education.map((edu, index) => (
-        <div key={index}>
-          <p>School Name: {edu.school}</p>
-          <p>Title: {edu.title}</p>
-          <p>Year: {edu.date}</p>
-        </div>
-      ))}
-      <h3>Experience</h3>
-      {formData.experience.map((exp, index) => (
-        <div key={index}>
-          <p>Company: {exp.company}</p>
-          <p>Position: {exp.title}</p>
-          <p>
-            From {exp.from_date} to {exp.to_date}
-          </p>
-          <p>Responsibilities: {exp.responsibilities}</p>
-        </div>
-      ))}
+    <>
+      <h2>Review</h2>
+      <div className="review" ref={targetRef}>
+        <h2>Curriculum Vitae</h2>
+        <p>
+          Name: {formData.first_name} {formData.last_name}
+        </p>
+        <h3>Contacts</h3>
+        <p>Email: {formData.email}</p>
+        <p>Phone Number: {formData.phone}</p>
+        <h3>Education</h3>
+        {formData.education.map((edu, index) => (
+          <div key={index}>
+            <p>School Name: {edu.school}</p>
+            <p>Title: {edu.title}</p>
+            <p>Year: {edu.date}</p>
+          </div>
+        ))}
+        <h3>Experience</h3>
+        {formData.experience.map((exp, index) => (
+          <div key={index}>
+            <p>Company: {exp.company}</p>
+            <p>Position: {exp.title}</p>
+            <p>
+              From {exp.from_date} to {exp.to_date}
+            </p>
+            <p>Responsibilities: {exp.responsibilities}</p>
+          </div>
+        ))}
+      </div>
       <button onClick={() => prevStep()}>Back</button>
-      <button>Download</button>
-    </div>
+      <button onClick={() => toPDF()}>Download</button>
+    </>
   );
 }
 
